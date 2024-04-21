2292f80861221cca4a6d8042efc1d09da04a30f0
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2024-04-21 06:46:58 -0400

    remove unecessary comptime checks

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,24 +1,3 @@
-comptime {
-    const builtin = @import("builtin");
-    const tgt = builtin.target;
-
-    // @Todo: Not sure if Abi.none is correct. IDK what System V ABI is in Zig
-    if (tgt.os.tag != .freestanding or tgt.cpu.arch != .x86 or tgt.abi != .none) {
-        @compileError("Target must be x86-freestanding-none (-target x86-freestanding-none)");
-    }
-
-    if (builtin.object_format != .elf) {
-        @compileError("Object format must be ELF (-ofmt=elf)");
-    }
-
-    const model_name = "i686";
-    if (tgt.cpu.model.name.len != model_name.len or for (tgt.cpu.model.name, 0..) |*c, i| {
-        if (c.* != model_name[i]) break true;
-    } else false) {
-        @compileError("Target cpu must be i686 (-mcpu i686)");
-    }
-}
-
 // Force boot.zig to be included in the build
 comptime {
     _ = @import("boot.zig");
